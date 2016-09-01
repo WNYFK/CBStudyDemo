3acf69ddee6a7fd76ef1d033b52930a9973a311d
陈斌 <chenbin05@meituan.com>
2016-09-01 09:53:47 +0800

    CBScrollView

---
+++ CBStudyDemo/CBStudyDemo/CBUIKit/CBScrollView.h
@@ -0,0 +1,13 @@
+//
+//  CBScrollView.h
+//  CBStudyDemo
+//
+//  Created by ChenBin on 16/8/31.
+//  Copyright © 2016年 ChenBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBScrollView : UIScrollView
+
+@end
---
+++ CBStudyDemo/CBStudyDemo/CBUIKit/CBScrollView.m
@@ -0,0 +1,13 @@
+//
+//  CBScrollView.m
+//  CBStudyDemo
+//
+//  Created by ChenBin on 16/8/31.
+//  Copyright © 2016年 ChenBin. All rights reserved.
+//
+
+#import "CBScrollView.h"
+
+@implementation CBScrollView
+
+@end
