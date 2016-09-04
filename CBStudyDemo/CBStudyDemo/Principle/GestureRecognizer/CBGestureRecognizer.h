84d4d4a01c617580667b37344e77a931d694334a
陈斌 <chenbin05@meituan.com>
2016-09-04 23:36:15 +0800

    手势

---
+++ CBStudyDemo/CBStudyDemo/Principle/GestureRecognizer/CBGestureRecognizer.h
@@ -0,0 +1,13 @@
+//
+//  CBGestureRecognizer.h
+//  CBStudyDemo
+//
+//  Created by chenbin on 16/9/4.
+//  Copyright © 2016年 ChenBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBGestureRecognizer : UIGestureRecognizer
+
+@end
---
+++ CBStudyDemo/CBStudyDemo/Principle/GestureRecognizer/CBGestureRecognizer.m
@@ -0,0 +1,125 @@
+//
+//  CBGestureRecognizer.m
+//  CBStudyDemo
+//
+//  Created by chenbin on 16/9/4.
+//  Copyright © 2016年 ChenBin. All rights reserved.
+//
+
+#import "CBGestureRecognizer.h"
+#import <UIKit/UIGestureRecognizerSubclass.h>
+
+@interface CBGestureRecognizer ()
+
+@property(nonatomic, assign) UIGestureRecognizerState state;
+
+@end
+
+@implementation CBGestureRecognizer
+@synthesize state = _state;
+
+- (instancetype)initWithTarget:(id)target action:(SEL)action
+{
+    if (self = [super initWithTarget:target action:action]) {
+        [self commonInitialization];
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    [self commonInitialization];
+}
+
+- (void)reset
+{
+    self.state = UIGestureRecognizerStatePossible;
+}
+
+- (void)commonInitialization
+{
+    self.cancelsTouchesInView = NO;
+    self.delaysTouchesBegan = YES;
+    
+    self.state = UIGestureRecognizerStatePossible;
+}
+
+- (void)setState:(UIGestureRecognizerState)state
+{
+    [super setState:state];
+    
+    if (state == UIGestureRecognizerStateFailed)
+        [self setEnableOtherGestureRecognizers:NO];
+    else
+        [self setEnableOtherGestureRecognizers:YES];
+}
+
+- (void)setEnableOtherGestureRecognizers:(BOOL)enabled
+{
+    if (self.view == nil)
+        return;
+    
+    for (UIGestureRecognizer* recognizer in self.view.gestureRecognizers)
+    {
+        if (recognizer == self) continue;
+        recognizer.enabled = enabled;
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+    
+    self.state = UIGestureRecognizerStatePossible;
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesEnded:touches withEvent:event];
+    
+    self.state = UIGestureRecognizerStateEnded;
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesCancelled:touches withEvent:event];
+    self.state = UIGestureRecognizerStateCancelled;
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesMoved:touches withEvent:event];
+    
+//    if (![self.view isKindOfClass:[MAYHorizontalListView class]]) return;
+    
+    if (self.state != UIGestureRecognizerStatePossible) return;
+    
+    UITouch *touch = [touches anyObject];
+    if (touch == nil) return;
+    
+    CGPoint location = [touch locationInView:self.view];
+    CGPoint prevLocation = [touch previousLocationInView:self.view];
+    
+    UIGestureRecognizerState newState = UIGestureRecognizerStatePossible;
+//    MAYHorizontalListView *listView = (MAYHorizontalListView *)self.view;
+    
+//    switch (self.type) {
+//            
+//        case MAYHorizontalListViewScrollRecognizerTypeFailedTouchingEdge: {
+//            if (listView.contentOffset.x <= 0 && location.x > prevLocation.x) {
+//                newState = UIGestureRecognizerStateFailed;
+//            } else if (listView.contentOffset.x >= listView.contentSize.width - listView.width && (location.x < prevLocation.x)) {
+//                newState = UIGestureRecognizerStateFailed;
+//            }
+//            break;
+//        }
+//        case MAYHorizontalListViewScrollRecognizerTypeAlwaysPossible:
+//        default:
+//            break;
+//    }
+    self.state = newState;
+}
+
+
+
+@end
