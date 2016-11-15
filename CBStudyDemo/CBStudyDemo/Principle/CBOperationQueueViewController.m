92183ace120ad00377868e6dc37836112ce0e14f
陈斌 <chenbin05@meituan.com>
2016-11-15 21:57:06 +0800

    blockoperation

--- CBStudyDemo/CBStudyDemo/Principle/CBOperationQueueViewController.m
+++ CBStudyDemo/CBStudyDemo/Principle/CBOperationQueueViewController.m
@@ -28,27 +28,59 @@
     
     @weakify(self);
     CBSectionItem *sectionItem = [[CBSectionItem alloc] init];
-    [sectionItem.cellItems addObject:[[CBSkipItem alloc] initWithTitle:@"BlockOperation" callBack:^{
+    dispatch_queue_t globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    [sectionItem.cellItems addObject:[[CBSkipItem alloc] initWithTitle:@"BlockOperation dispatch_async" callBack:^{
         @strongify(self);
         NSBlockOperation *blockOperation1 = [NSBlockOperation blockOperationWithBlock:^{
-            NSLog(@"blockOperation1 start");
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                NSLog(@"blockOperation1 dispatch after finished");
+            NSLog(@"cell1 blockOperation1 start");
+            dispatch_async(globalQueue, ^{
+                NSLog(@"cell1 blockOperation1 globalQueue");
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 15), globalQueue, ^{
+                    NSLog(@"cell1 blockOperation1 dispatch after finished");
+                });
             });
-            NSLog(@"blockOperation1 end");
+            
+            NSLog(@"cell1 blockOperation1 end");
         }];
         
         NSBlockOperation *blockOperation2 = [NSBlockOperation blockOperationWithBlock:^{
-            NSLog(@"blockOperation2 start");
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                NSLog(@"blockOperation2 dispatch after finished");
+            NSLog(@"cell1 blockOperation2 start");
+            dispatch_async(globalQueue, ^{
+                NSLog(@"cell1 blockOperation2 global");
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2), globalQueue, ^{
+                    NSLog(@"cell1 blockOperation2 dispatch after finished");
+                });
             });
-            NSLog(@"blockOperation2 end");
+            
+            NSLog(@"cell1 blockOperation2 end");
         }];
         [blockOperation2 addDependency:blockOperation1];
         [self.blockOperationQueue addOperation:blockOperation1];
         [self.blockOperationQueue addOperation:blockOperation2];
     }]];
+    
+    [sectionItem.cellItems addObject:[[CBSkipItem alloc] initWithTitle:@"BlockOperation dispatch_after" callBack:^{
+        @strongify(self);
+        NSBlockOperation *blockOperation1 = [NSBlockOperation blockOperationWithBlock:^{
+            NSLog(@"cell2 blockOperation1 start");
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 15), globalQueue, ^{
+                NSLog(@"cell2 blockOperation1 dispatch after finished");
+            });
+            NSLog(@"cell2 blockOperation1 end");
+        }];
+        
+        NSBlockOperation *blockOperation2 = [NSBlockOperation blockOperationWithBlock:^{
+            NSLog(@"cell2 blockOperation2 start");
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2), globalQueue, ^{
+                NSLog(@"cell2 blockOperation2 dispatch after finished");
+            });
+            NSLog(@"cell2 blockOperation2 end");
+        }];
+        [blockOperation2 addDependency:blockOperation1];
+        [self.blockOperationQueue addOperation:blockOperation1];
+        [self.blockOperationQueue addOperation:blockOperation2];
+    }]];
+    [self.dataArr addObject:sectionItem];
 }
 
 @end
